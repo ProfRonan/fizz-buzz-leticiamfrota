76ba25fc839602d3db558f12e293be2b2423d8d5
leticiamfrota <leticiafrotamesquita@alu.ufc.br>
2023-03-31 10:39:46 -0300

    Exercício Concluído

---
+++ main.py
@@ -0,0 +1,12 @@
+número = int(input('Digite um número: '))
+
+if número % 3 == 0 and número % 5 == 0:
+    print('FizzBuzz')
+
+else:
+    if número % 3 == 0:
+        print('Fizz')
+    elif número % 5 == 0:
+        print('Buzz')
+    else:
+        print(número)
